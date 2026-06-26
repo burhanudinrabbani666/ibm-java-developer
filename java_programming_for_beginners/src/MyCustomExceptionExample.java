5541d7956ccf25a15ccec3e9a9f0141038a0b828
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-26 19:48:10 +0700

    Exception and error handling

---
+++ java_programming_for_beginners/src/MyCustomExceptionExample.java
@@ -0,0 +1,26 @@
+package java_programming_for_beginners.src;
+
+import java.lang.Exception;
+
+/**
+ * InnerException
+ */
+class MyCustomException extends Exception {
+
+    public MyCustomException(String message) {
+        super(message);
+    }
+}
+
+public class MyCustomExceptionExample {
+
+    public static void main(String[] args) {
+
+        try {
+            throw new MyCustomException("This is a custom exception mesage");
+        } catch (MyCustomException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
