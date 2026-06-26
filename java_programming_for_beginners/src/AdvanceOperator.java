7a53ee7f01a21b19e797d71e991ecd6d71841dad
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-26 14:22:17 +0700

    Advance operator

---
+++ java_programming_for_beginners/src/AdvanceOperator.java
@@ -0,0 +1,20 @@
+package java_programming_for_beginners.src;
+
+public class AdvanceOperator {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        a += 5;
+        System.err.println("New Value of a: " + a);
+
+        a *= 2;
+        System.err.println("After multiplication, new value of a: " + a);
+
+        // Unary operator
+        /**
+         * ++
+         * --
+         */
+
+    }
+}
