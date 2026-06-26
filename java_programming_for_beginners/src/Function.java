a506232a6471ad6cab086bb8b9a469545501ef8d
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-26 19:02:31 +0700

    Simple calculator

---
+++ java_programming_for_beginners/src/Function.java
@@ -0,0 +1,14 @@
+package java_programming_for_beginners.src;
+
+public class Function {
+    public static float addNums(float a, float b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        float sum = addNums(12, 12);
+        System.out.println(sum);
+
+    }
+}
